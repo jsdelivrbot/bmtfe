2628716b4aeed64dc08e9dd546e27c6959ef8c20
Marco Alfaro <marco.alfaro@gmail.com>
2016-11-11 21:18:17 -0600

    anything

--- web.js
+++ web.js
@@ -1,7 +1,11 @@
 var gzippo = require('gzippo');
 var express = require('express');
+
+var morgan = require('morgan');
+var logger = morgan('combined')
+
 var app = express();
 
-app.use(express.logger('dev'));
+app.use(morgan('combined'));
 app.use(gzippo.staticGzip("" + __dirname + "/build"));
 app.listen(process.env.PORT || 5000);
